4f9db7a78370c28b59c547a760f92172989085f2
Zin Lin Phyo <zinlinphyo.mm@gmail.com>
2017-11-04 17:51:18 +0630

    Initialize skeleton projects with default structure / value.

---
+++ app/src/main/java/net/zinlinphyo/mm_news/SFCNewsApp.java
@@ -0,0 +1,17 @@
+package net.zinlinphyo.mm_news;
+
+import android.app.Application;
+
+/**
+ * Created by zinlinphyo on 11/4/17.
+ */
+
+public class SFCNewsApp extends Application {
+
+    public static final String LOG_TAG = "SFCNewsApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
